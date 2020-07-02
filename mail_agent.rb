f86a67fa890b2ca049e407173644b40fe3f823da
kurilaa <kurilaa@logicon-group.ru>
2020-07-02 10:58:41 +0500

    html_part check added

--- mail_agent.rb
+++ mail_agent.rb
@@ -4,7 +4,7 @@ Mail.defaults do
   retriever_method :imap, :address    => "imap.yandex.ru",
                           :port       => 993,
                           :user_name  => 'kurilaa@logicon-group.ru',
-                          :password   => 'password',
+                          :password   => 'rjisebtxolanqubf',
                           :enable_ssl => true
 end
 
@@ -13,5 +13,10 @@ sender = email.from
 subject = email.subject
 body = email.body.decoded
 plain_part = email.text_part ? email.text_part.body.decoded : nil
+html_part = email.html_part ? email.html_part.body.decoded : nil
 puts "SENDER: #{sender}\nSUBJECT: #{subject}"
-puts "MESSAGE:\n#{plain_part}"
+if plain_part.nil?
+  puts "HTML MESSAGE: #{html_part.nil?}"
+else
+  puts "PLAIN MESSAGE: #{plain_part.nil?}"
+end
